7369775d3f72a1738477ca4f5bafacac02ef0ae3
Justin Jay Taller <jayrell012@yahoo.com>
2020-03-18 09:26:50 +0800

    init commit

---
+++ Xamarin/App1/App1/Data/TokenDatabaseController.cs
@@ -0,0 +1,61 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using App1.Models;
+
+namespace App1.Data
+{
+    public class TokenDatabaseController
+    {
+        static object locker = new object();
+
+        SQLiteConnection database;
+
+        public TokenDatabaseController()
+        {
+            database = DependencyService.Get<ISQLite>().GetConnection();
+            database.CreateTable<Token>();
+        }
+
+        public Token GetUser()
+        {
+            lock (locker)
+            {
+                if (database.Table<Token>().Count() == 0)
+                {
+                    return null;
+                }
+                else
+                {
+                    return database.Table<Token>().First();
+                }
+            }
+        }
+
+        public int SaveUser(Token token)
+        {
+            lock (locker)
+            {
+                if (token.Id != 0)
+                {
+                    database.Update(token);
+                    return token.Id;
+                }
+                else
+                {
+                    return database.Insert(token);
+                }
+            }
+        }
+
+        public int DeleteUser(int id)
+        {
+            lock (locker)
+            {
+                return database.Delete<Token>(id);
+            }
+        }
+    }
+}
---
+++ Xamarin/App1/App1/Views/LoginPage.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using App1.Models;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class LoginPage : ContentPage
+	{
+		public LoginPage ()
+		{
+			InitializeComponent ();
+			Init ();
+		}
+
+        void Init()
+        {
+            BackgroundColor = Constants.backgroundColor;
+            Lbl_Password.TextColor = Constants.mainTextColor;
+            Lbl_Username.TextColor = Constants.mainTextColor;
+            ActivitySpinner.IsVisible = false;
+            LoginIcon.HeightRequest = Constants.LoginIconHeight;
+            LoginIcon.WidthRequest = Constants.LoginIconWidth;
+            Entry_Username.BackgroundColor = Constants.entryBackgroundColor;
+            Entry_Username.TextColor = Constants.entryMainTextColor;
+            Entry_Password.BackgroundColor = Constants.entryBackgroundColor;
+            Entry_Password.TextColor = Constants.entryMainTextColor;
+            Btn_Signin.BackgroundColor = Constants.buttonBackgroundColor;
+            Btn_Signin.TextColor = Constants.buttonMainTextColor;
+
+            Entry_Username.Completed += (s, e) => Entry_Password.Focus();
+            Entry_Password.Completed += (s, e) => SignInProcedure(s, e);
+            Entry_Password.IsPassword = true;
+        }
+        void SignInProcedure(object sender, EventArgs e)
+        {
+            try
+            {
+                User user = new User(Entry_Username.Text, Entry_Password.Text);
+                if (user.CheckInformation())
+                {
+                    DisplayAlert("Login", "Login Success", "OK");
+                    ActivitySpinner.IsVisible = true;
+                    Entry_Username.TextColor = Constants.entryMainTextColor;
+                    Entry_Password.TextColor = Constants.entryMainTextColor;
+                    Entry_Username.BackgroundColor = Constants.backgroundColor;
+                    Entry_Password.BackgroundColor = Constants.backgroundColor;
+
+                    App.UserDatabase.SaveUser(user);
+                }
+                else
+                {
+                    DisplayAlert("Login", "Login Not Correct", "OK");
+                }
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Error Message",ex.Message,"Sad");
+            }
+        }
+    }
+}
