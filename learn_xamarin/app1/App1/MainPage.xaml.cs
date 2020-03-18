b79eb24fe40f03fd4f47d4ffc9bfa37623c9d0e6
Justin Jay Taller <jayrell012@yahoo.com>
2020-03-18 10:25:42 +0800

    initial commit

---
+++ learn_xamarin/app1/App1/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        int count = 0;
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            count++;
+
+            ((Button)sender).Text = $"You clicked {count} times";
+        }
+    }
+}
