fd13b7c95c9229116e429790f74f4e810a983a9e
Justin Jay Taller <jayrell012@yahoo.com>
2020-03-18 09:42:31 +0800

    init commit

---
+++ Xamarin_Repo/app1/App1.iOS/Data/SQLite_IOS.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using App1.Data;
+using App1.iOS.Data;
+using Foundation;
+using UIKit;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(SQLite_IOS))]
+
+namespace App1.iOS.Data
+{
+    public class SQLite_IOS : ISQLite
+    {
+        public SQLite_IOS() { }
+        public SQLite.SQLiteConnection GetConnection()
+        {
+            var fileName = "TestDB.db3";
+            string documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            var libraryPath = System.IO.Path.Combine(documentsPath, "..", "Library");
+            var path = System.IO.Path.Combine(libraryPath, fileName);
+            var connection = new SQLite.SQLiteConnection(path);
+
+            return connection;
+        }
+    }
+}
